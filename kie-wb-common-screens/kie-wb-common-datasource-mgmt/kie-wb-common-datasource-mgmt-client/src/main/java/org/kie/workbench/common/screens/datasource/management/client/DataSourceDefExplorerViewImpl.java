3e78108cfc04c68f64504c8e0406b7627f8bf640
Walter Medvedeo <wmedvedeo@gmail.com>
2016-02-26 18:58:50 +0100

    Data sources management (experimental) I

---
+++ kie-wb-common-screens/kie-wb-common-datasource-mgmt/kie-wb-common-datasource-mgmt-client/src/main/java/org/kie/workbench/common/screens/datasource/management/client/DataSourceDefExplorerViewImpl.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.screens.datasource.management.client;
+
+import javax.enterprise.context.Dependent;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import org.gwtbootstrap3.client.ui.LinkedGroup;
+
+@Dependent
+public class DataSourceDefExplorerViewImpl
+        extends Composite
+        implements DataSourceDefExplorerView {
+
+    interface Binder extends UiBinder<Widget, DataSourceDefExplorerViewImpl> {
+
+    }
+
+    private static Binder uiBinder = GWT.create( Binder.class );
+
+    @UiField
+    com.google.gwt.user.client.ui.Label emptyLabel;
+
+    @UiField
+    LinkedGroup itemsGroup;
+
+    private Presenter presenter;
+
+    public DataSourceDefExplorerViewImpl() {
+        initWidget( uiBinder.createAndBindUi( this ) );
+    }
+
+    @Override
+    public void init( Presenter presenter ) {
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void addItem( DataSourceDefItem item ) {
+        itemsGroup.add( item );
+    }
+
+    @Override
+    public void removeItem( DataSourceDefItem item ) {
+        itemsGroup.remove( item );
+    }
+
+    @Override
+    public void clear() {
+        itemsGroup.clear();
+    }
+}
