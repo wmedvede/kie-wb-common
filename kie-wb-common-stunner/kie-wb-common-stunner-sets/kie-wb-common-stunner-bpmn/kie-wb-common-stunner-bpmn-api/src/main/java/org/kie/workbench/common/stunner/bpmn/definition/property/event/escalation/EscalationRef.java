5f15705383003ad56cf21613a96fd086f35ba7c9
Walter Medvedeo <wmedvedeo@gmail.com>
2018-07-24 08:49:05 +0200

    start escalation event I

--- kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-api/src/main/java/org/kie/workbench/common/stunner/bpmn/definition/property/event/escalation/EscalationRef.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-api/src/main/java/org/kie/workbench/common/stunner/bpmn/definition/property/event/escalation/EscalationRef.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kie.workbench.common.stunner.bpmn.definition.property.event.signal;
+
+package org.kie.workbench.common.stunner.bpmn.definition.property.event.escalation;
+
+import java.util.Objects;
 
 import org.jboss.errai.common.client.api.annotations.MapsTo;
 import org.jboss.errai.common.client.api.annotations.Portable;
@@ -24,22 +27,23 @@ import org.kie.workbench.common.forms.adf.definitions.annotations.metaModel.I18n
 import org.kie.workbench.common.stunner.bpmn.definition.BPMNProperty;
 import org.kie.workbench.common.stunner.core.definition.annotation.Property;
 import org.kie.workbench.common.stunner.core.definition.annotation.property.Value;
+import org.kie.workbench.common.stunner.core.util.HashUtil;
 
 @Portable
 @Bindable
 @Property
 @FieldDefinition(i18nMode = I18nMode.OVERRIDE_I18N_KEY)
-public class SignalScope implements BPMNProperty {
+public class EscalationRef implements BPMNProperty {
 
     @Value
     @FieldValue
     private String value;
 
-    public SignalScope() {
+    public EscalationRef() {
         this("");
     }
 
-    public SignalScope(final @MapsTo("value") String value) {
+    public EscalationRef(final @MapsTo("value") String value) {
         this.value = value;
     }
 
@@ -53,14 +57,14 @@ public class SignalScope implements BPMNProperty {
 
     @Override
     public int hashCode() {
-        return (null != value) ? value.hashCode() : 0;
+        return HashUtil.combineHashCodes(Objects.hashCode(value));
     }
 
     @Override
     public boolean equals(Object o) {
-        if (o instanceof SignalScope) {
-            SignalScope other = (SignalScope) o;
-            return (null != value) ? value.equals(other.value) : null == other.value;
+        if (o instanceof EscalationRef) {
+            EscalationRef other = (EscalationRef) o;
+            return Objects.equals(value, other.value);
         }
         return false;
     }
